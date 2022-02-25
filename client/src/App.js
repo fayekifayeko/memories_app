2c8a1052a5900b9496670574ab98a03543d24cf7
Slim Faeq <faeq.Slim@diaverum.com>
2022-02-25 20:33:54 +0100

    start with auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
-import memories from "./images/memories.png";
+import { Container, Grow, Grid } from "@material-ui/core";
 import Form from "./components/Form/Form.js";
 import Posts from "./components/Posts/Posts.js";
 import useStyles from "./styles.js";
 import { useDispatch } from "react-redux";
 import { getPosts } from "./actions/posts";
+import NavBar from "./components/NavBar/NavBar.js";
 
 const App = () => {
   const classes = useStyles();
@@ -18,17 +18,7 @@ const App = () => {
 
   return (
     <Container maxidth="lg">
-      <AppBar className={classes.appBar} position="static" color="inherit">
-        <Typography className={classes.heading} variant="h2" align="center">
-          Memories
-        </Typography>
-        <img
-          className={classes.image}
-          src={memories}
-          alt="Memories"
-          height="60"
-        />
-      </AppBar>
+      <NavBar />
       <Grow in>
         <Container>
           <Grid
